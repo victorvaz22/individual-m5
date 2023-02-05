4c2ab86ce5096cfc067bb955510046b9db425630
Victor Alves Vaz <ckvictor.vaz@hotmail.com>
2023-02-05 13:17:00 -0300

    projeto att

--- ordernador-propriedades-css/app.js
+++ ordernador-propriedades-css/app.js
@@ -5,7 +5,7 @@ var listaPropriedadesCss = [];
 var inputPropriedade = "";
 var isRunning = true;
 
-console.log("Seja bem vindo ao ordenador de propriedades CSS, você pode inserir quantas vocês quiser abaixo e digitar SAIR para exibir a lista de propriedade passadas de forma ordenada.")
+console.log("Bem vindo ao ordenador de propriedades CSS, você pode inserir quantas vocês quiser e digitar SAIR para exibir a lista de propriedade passadas de forma ordenada.")
 while(inputPropriedade !== "SAIR"){
     inputPropriedade = prompt('Insira o nome da propriedade CSS ou digite exatamente SAIR: ');
         if(isRunning === true) {
